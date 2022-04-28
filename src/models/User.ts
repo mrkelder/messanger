03f21c16c2d76bb2ddc9a2412db2a65ffb6a80a8
Vlad A <vageev06@gmail.com>
2022-04-28 12:44:00 +0300

    fix: added interfaces and one more static to User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,16 @@
-import { Schema, models, model } from "mongoose";
+import { Schema, models, model, Document, Model } from "mongoose";
 
 import { USER_MODEL_NAME } from "./CONSTANTS";
+
+interface UserDocument extends Document {
+  name: string;
+  password: string;
+}
+
+interface UserModel extends Model<UserDocument> {
+  findByName(name: string): Promise<Array<UserDocument>>;
+  deleteByName(name: string): Promise<void>;
+}
 
 const userSchema = new Schema({
   name: String,
@@ -11,4 +21,9 @@ userSchema.static("findByName", async function (name: string) {
   return await this.find({ name });
 });
 
-export default models[USER_MODEL_NAME] || model(USER_MODEL_NAME, userSchema);
+userSchema.static("deleteByName", async function (name: string) {
+  await this.deleteOne({ name });
+});
+
+export default (models[USER_MODEL_NAME] as UserModel) ||
+  model<UserModel>(USER_MODEL_NAME, userSchema);
