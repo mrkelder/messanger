ab801e440b1cec3256c1360d1ae620dd666b5966
Vlad A <vageev06@gmail.com>
2022-04-30 18:21:59 +0300

    fix: made authorizate endpoint look better

--- pages/api/auth/authorizate.ts
+++ pages/api/auth/authorizate.ts
@@ -3,34 +3,80 @@ import mongoose from "mongoose";
 import { NextApiRequest, NextApiResponse } from "next";
 
 import User from "src/models/User";
+import { Credentials } from "src/types/auth";
+import { DatabaseUser } from "src/types/db";
+import RequestHelper from "src/utils/RequestHelper";
+
+class Authorization {
+  private name: string;
+  private password: string;
+  private response: NextApiResponse;
+  private foundUsers: DatabaseUser[] = [];
+
+  constructor(credentials: Credentials, res: NextApiResponse) {
+    const { name, password } = credentials;
+    this.name = name;
+    this.password = password;
+    this.response = res;
+  }
+
+  public async run() {
+    if (this.areNameAndPasswordDefined()) await this.nameAndPasswordDefined();
+    else this.nameOrPasswordUndefined();
+  }
+
+  private areNameAndPasswordDefined(): boolean {
+    return !!this.name && !!this.password;
+  }
+
+  private nameOrPasswordUndefined() {
+    this.response.status(403).send("Either name or password was not provided");
+  }
+
+  private async nameAndPasswordDefined() {
+    await this.lookForUsers();
+    if (this.foundUsers.length > 0) await this.userExists();
+    else this.userDoesNotExist();
+  }
+
+  private async lookForUsers() {
+    this.foundUsers = await User.findByName(this.name);
+  }
+
+  private userDoesNotExist() {
+    this.response.status(404).send("User was not found");
+  }
+
+  private async userExists() {
+    if (await this.areCredentialsEqual()) this.credentialsAreEqual();
+    else this.credentialsAreNotEqual();
+  }
+
+  public async areCredentialsEqual(): Promise<boolean> {
+    return await bcrypt.compare(this.password, this.foundUsers[0].password);
+  }
+
+  public credentialsAreEqual() {
+    this.response.status(200).json(this.foundUsers[0]);
+  }
+
+  public credentialsAreNotEqual() {
+    this.response.status(403).send("Password is not correct");
+  }
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method?.toLocaleLowerCase() === "post") {
+  const requestHelper = new RequestHelper(req);
+
+  if (requestHelper.isPOST()) {
     try {
-      const { name, password } = JSON.parse(req.body);
-
-      if (!name || !password) {
-        res.status(403).send("Either name or password was not provided");
-        return;
-      }
-
+      const credentials = requestHelper.getBody();
       await mongoose.connect(process.env.MONGODB_HOST as string);
-
-      const lookUpUsers = await User.findByName(name);
-
-      if (lookUpUsers.length > 0) {
-        const [lookUpUser] = lookUpUsers;
-        const isPasswordEqual = await bcrypt.compare(
-          password,
-          lookUpUser.password
-        );
-
-        if (isPasswordEqual) res.status(200).json(lookUpUser);
-        else res.status(403).send("Password is not correct");
-      } else res.status(404).send("User was not found");
+      const authorization = new Authorization(credentials, res);
+      await authorization.run();
     } catch (err) {
       res.status(500).send("Server could not handle the request");
     } finally {
