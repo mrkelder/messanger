29d0a5bd82ed41d75b180983e998df87c20d6a97
Vlad A <vageev06@gmail.com>
2022-05-01 11:08:32 +0300

    test: changed JWT behavior

--- src/utils/JWT/JWT.test.ts
+++ src/utils/JWT/JWT.test.ts
@@ -1,41 +1,25 @@
 import JWT from "./JWT";
-import { AccessRefreshSecrets, TokenPayload } from "./JWTTypes";
 
-const tokenPair: AccessRefreshSecrets = {
-  access: "access-token",
-  refresh: "refresh-token"
-};
-
-const tokenPayload: TokenPayload = { _id: "507f191e810c19729de860ea" };
-
-const jwt = new JWT(tokenPair, tokenPayload);
+const userId = "507f191e810c19729de860ea";
 
 describe("Json Web Token class", () => {
   let accessToken: string;
   let refreshToken: string;
 
   test("Should result in an access token", () => {
-    accessToken = jwt.createAccessToken();
+    accessToken = JWT.createAccessToken(userId);
     expect(accessToken).toBeDefined();
   });
 
   test("Should result in a refresh token", () => {
-    refreshToken = jwt.createRefreshToken();
+    refreshToken = JWT.createRefreshToken(userId);
     expect(refreshToken).toBeDefined();
   });
 
   test("Should validate access and refresh tokens successfully", () => {
-    const accessTokenValidation = jwt.validateAccessToken(accessToken);
-    const refreshTokenValidation = jwt.validateRefreshToken(refreshToken);
-    expect(accessTokenValidation).toBeTruthy();
-    expect(refreshTokenValidation).toBeTruthy();
-  });
-
-  test("Should extract the same _id from both access and refresh tokens' payloads", () => {
-    const { _id: accessTokenId } = JWT.extractTokenPayload(accessToken);
-    const { _id: refreshTokenId } = JWT.extractTokenPayload(refreshToken);
-
-    expect(accessTokenId).toBe(tokenPayload._id);
-    expect(refreshTokenId).toBe(tokenPayload._id);
+    expect(() => {
+      JWT.verifyAccessToken(accessToken);
+      JWT.verifyRefreshToken(refreshToken);
+    }).not.toThrow();
   });
 });
