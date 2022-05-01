c48c3f9eb44c1c12c146081527fb1fcb2f7d2f01
Vlad A <vageev06@gmail.com>
2022-05-01 16:03:53 +0300

    test: added JWT requirement to auth logic

--- __test__/api/auth.test.ts
+++ __test__/api/auth.test.ts
@@ -12,25 +12,18 @@ const testUser = {
   name: "test-user-name-that-mustn't-be-copied",
   password: "some-password"
 };
-
-const testUserBody = JSON.stringify(testUser);
-const testUserWithoutNameBody = JSON.stringify({ password: testUser.password });
-const testUserWithoutPasswordBody = JSON.stringify({ name: testUser.name });
-
-const testUserWithWrongNameBody = JSON.stringify({
-  password: testUser.password,
-  name: "some-other-user"
-});
-const testUserWithWrongPasswordBody = JSON.stringify({
-  name: testUser.name,
-  password: "some-other-password"
-});
+const testUserWithoutName = { password: testUser.password };
+const testUserWithoutPassword = { name: testUser.name };
 
 jest.setTimeout(15 * 1000);
 
+async function deleteUser() {
+  await User.deleteByName(testUser.name);
+}
+
 beforeAll(async () => {
   await mongoose.connect("mongodb://127.0.0.1:27017/messenger");
-  await User.deleteByName(testUser.name);
+  await deleteUser();
 });
 
 afterAll(async () => {
@@ -48,17 +41,25 @@ describe("Registration", () => {
 
   test("Should throw an error because of an unspecified name", async () => {
     try {
-      await axios.post(registrateAPI, testUserWithoutNameBody, conf);
+      await axios.post(
+        registrateAPI,
+        JSON.stringify(testUserWithoutName),
+        conf
+      );
     } catch ({ message }) {
-      expect(message).toMatch("403");
+      expect(message).toMatch("500");
     }
   });
 
   test("Should throw an error because of an unspecified password", async () => {
     try {
-      await axios.post(registrateAPI, testUserWithoutPasswordBody, conf);
+      await axios.post(
+        registrateAPI,
+        JSON.stringify(testUserWithoutPassword),
+        conf
+      );
     } catch ({ message }) {
-      expect(message).toMatch("403");
+      expect(message).toMatch("500");
     }
   });
 
@@ -71,13 +72,18 @@ describe("Registration", () => {
   });
 
   test("Should successfully registrate a user", async () => {
-    const result = await axios.post(registrateAPI, testUserBody, conf);
+    const result = await axios.post(
+      registrateAPI,
+      JSON.stringify(testUser),
+      conf
+    );
     expect(result.status).toBe(200);
+    expect(result.data.accessToken).toBeDefined();
   });
 
   test("Should throw an error because of other the user having the same name", async () => {
     try {
-      await axios.post(registrateAPI, testUserBody, conf);
+      await axios.post(registrateAPI, JSON.stringify(testUser), conf);
     } catch ({ message }) {
       expect(message).toMatch("409");
     }
@@ -85,6 +91,46 @@ describe("Registration", () => {
 });
 
 describe("Authorization", () => {
+  beforeEach(async () => {
+    await deleteUser();
+  });
+
+  async function registrateUser(): Promise<string> {
+    const result = await axios.post(
+      registrateAPI,
+      JSON.stringify(testUser),
+      conf
+    );
+    return result.data.accessToken;
+  }
+
+  const testUserWithAccessToken = (accessToken: string) => ({
+    ...testUser,
+    accessToken
+  });
+
+  const testUserWithoutNameWithAccessToken = (accessToken: string) => ({
+    ...testUserWithoutName,
+    accessToken
+  });
+
+  const testUserWithoutPasswordWithAccessToken = (accessToken: string) => ({
+    ...testUserWithoutPassword,
+    accessToken
+  });
+
+  const testUserWithWrongNameWithAccessToken = (accessToken: string) => ({
+    password: testUser.password,
+    name: "some-other-user",
+    accessToken
+  });
+
+  const testUserWithWrongPasswordWithAccessToken = (accessToken: string) => ({
+    name: testUser.name,
+    password: "some-other-password",
+    accessToken
+  });
+
   test("Should throw an error because of the wrong http method", async () => {
     try {
       await axios.get(authorizateAPI);
@@ -95,23 +141,38 @@ describe("Authorization", () => {
 
   test("Should throw an error because of an unspecified name", async () => {
     try {
-      await axios.post(authorizateAPI, testUserWithoutNameBody, conf);
+      const accessToken = await registrateUser();
+      await axios.post(
+        authorizateAPI,
+        JSON.stringify(testUserWithoutNameWithAccessToken(accessToken)),
+        conf
+      );
     } catch ({ message }) {
-      expect(message).toMatch("403");
+      expect(message).toMatch("500");
     }
   });
 
   test("Should throw an error because of an unspecified password", async () => {
     try {
-      await axios.post(authorizateAPI, testUserWithoutPasswordBody, conf);
+      const accessToken = await registrateUser();
+      await axios.post(
+        authorizateAPI,
+        JSON.stringify(testUserWithoutPasswordWithAccessToken(accessToken)),
+        conf
+      );
     } catch ({ message }) {
-      expect(message).toMatch("403");
+      expect(message).toMatch("500");
     }
   });
 
   test("Should throw an error because such user doesn't exist", async () => {
     try {
-      await axios.post(authorizateAPI, testUserWithWrongNameBody, conf);
+      const accessToken = await registrateUser();
+      await axios.post(
+        authorizateAPI,
+        JSON.stringify(testUserWithWrongNameWithAccessToken(accessToken)),
+        conf
+      );
     } catch ({ message }) {
       expect(message).toMatch("404");
     }
@@ -119,21 +180,45 @@ describe("Authorization", () => {
 
   test("Should throw an error because of the wrong password", async () => {
     try {
-      await axios.post(authorizateAPI, testUserWithWrongPasswordBody, conf);
+      const accessToken = await registrateUser();
+      await axios.post(
+        authorizateAPI,
+        JSON.stringify(testUserWithWrongPasswordWithAccessToken(accessToken)),
+        conf
+      );
     } catch ({ message }) {
-      expect(message).toMatch("403");
+      expect(message).toMatch("401");
     }
   });
 
   test("Should result in a successful authorization", async () => {
-    const result = await axios.post(authorizateAPI, testUserBody, conf);
+    const accessToken = await registrateUser();
+    const result = await axios.post(
+      authorizateAPI,
+      JSON.stringify(testUserWithAccessToken(accessToken)),
+      conf
+    );
     expect(result.status).toBe(200);
+  });
+
+  test("Should throw an error due to unspecified access token", async () => {
+    try {
+      await registrateUser();
+      await axios.post(authorizateAPI, JSON.stringify(testUser), conf);
+    } catch ({ message }) {
+      expect(message).toMatch("500");
+    }
   });
 
   test("Should throw an error because the user is deleted", async () => {
     try {
-      await User.deleteByName(testUser.name);
-      await axios.post(authorizateAPI, testUserBody, conf);
+      const accessToken = await registrateUser();
+      await deleteUser();
+      await axios.post(
+        authorizateAPI,
+        JSON.stringify(testUserWithAccessToken(accessToken)),
+        conf
+      );
     } catch ({ message }) {
       expect(message).toMatch("404");
     }
