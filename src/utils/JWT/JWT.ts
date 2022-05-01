e7ea5aa293b8d0c06b4a53eb9afe3ceb2ec4305e
Vlad A <vageev06@gmail.com>
2022-05-01 11:08:47 +0300

    fix: updated JWT class to new requirements

--- src/utils/JWT/JWT.ts
+++ src/utils/JWT/JWT.ts
@@ -1,54 +1,45 @@
-import jsonwebtoken from "jsonwebtoken";
+import jsonwebtoken, { JwtPayload } from "jsonwebtoken";
 
-import { AccessRefreshSecrets, TokenPayload } from "./JWTTypes";
+const ONE_YEAR_IN_MILLISECONDS = 31556952000;
+const TEN_MINUTES_IN_MILLISECONDS = 600000;
 
 class JWT {
-  private static expiresIn = 1000 * 60 * 10;
+  private static accessExpirational = TEN_MINUTES_IN_MILLISECONDS;
+  private static refreshExpirational = ONE_YEAR_IN_MILLISECONDS;
+  private static accessSecret = process.env.ACCESS_TOKEN_SECRET as string;
+  private static refreshSecret = process.env.REFRESH_TOKEN_SECRET as string;
 
-  private accessSecret: string;
-  private refreshSecret: string;
-  private payload: TokenPayload;
-
-  constructor(tokens: AccessRefreshSecrets, payload: TokenPayload) {
-    const { access, refresh } = tokens;
-    this.accessSecret = access;
-    this.refreshSecret = refresh;
-    this.payload = payload;
+  public static createAccessToken(userId: string): string {
+    return JWT.createToken(userId, JWT.accessSecret, JWT.accessExpirational);
   }
 
-  public createAccessToken(): string {
-    return this.createToken(this.accessSecret);
+  public static createRefreshToken(userId: string): string {
+    return JWT.createToken(userId, JWT.refreshSecret, JWT.refreshExpirational);
   }
 
-  public createRefreshToken(): string {
-    return this.createToken(this.refreshSecret);
-  }
-
-  private createToken(token: string): string {
-    return jsonwebtoken.sign(this.payload, token, {
-      expiresIn: JWT.expiresIn
+  private static createToken(
+    userId: string,
+    token: string,
+    expiresIn: number
+  ): string {
+    return jsonwebtoken.sign({ _id: userId }, token, {
+      expiresIn
     });
   }
 
-  public validateAccessToken(accessToken: string): boolean {
-    return this.validateToken(accessToken, this.accessSecret);
+  public static verifyAccessToken(accessToken: string): string | JwtPayload {
+    return JWT.validateToken(accessToken, JWT.accessSecret);
   }
 
-  public validateRefreshToken(refreshToken: string): boolean {
-    return this.validateToken(refreshToken, this.refreshSecret);
+  public static verifyRefreshToken(refreshToken: string): string | JwtPayload {
+    return JWT.validateToken(refreshToken, JWT.refreshSecret);
   }
 
-  private validateToken(token: string, secret: string): boolean {
-    try {
-      const result = jsonwebtoken.verify(token, secret);
-      return !!result;
-    } catch {
-      return false;
-    }
-  }
-
-  public static extractTokenPayload(token: string): TokenPayload {
-    return new Object(jsonwebtoken.decode(token)) as TokenPayload;
+  private static validateToken(
+    token: string,
+    secret: string
+  ): string | JwtPayload {
+    return jsonwebtoken.verify(token, secret);
   }
 }
 
