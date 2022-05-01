4f460a9cd775d0d4c2853e2cc5a70ef8ef2036a0
Vlad A <vageev06@gmail.com>
2022-05-01 16:04:36 +0300

    fix: added authentication (sing in/up)

--- pages/api/auth/authorizate.ts
+++ pages/api/auth/authorizate.ts
@@ -5,85 +5,122 @@ import { NextApiRequest, NextApiResponse } from "next";
 import User from "src/models/User";
 import { Credentials } from "src/types/auth";
 import { DatabaseUser } from "src/types/db";
+import JWT from "src/utils/JWT";
 import RequestHelper from "src/utils/RequestHelper";
-
-class Authorization {
-  private name: string;
-  private password: string;
-  private response: NextApiResponse;
-  private foundUser: DatabaseUser | undefined;
-
-  constructor(credentials: Credentials, res: NextApiResponse) {
-    const { name, password } = credentials;
-    this.name = name;
-    this.password = password;
-    this.response = res;
-  }
-
-  public async run() {
-    if (this.areNameAndPasswordDefined()) await this.checkIfUserExists();
-    else this.throwNameOrPasswordUndefinedError();
-  }
-
-  private areNameAndPasswordDefined(): boolean {
-    return !!this.name && !!this.password;
-  }
-
-  private throwNameOrPasswordUndefinedError() {
-    this.response.status(403).send("Either name or password was not provided");
-  }
-
-  private async checkIfUserExists() {
-    await this.lookForUser();
-    if (this.foundUser) await this.checkCredentials();
-    else this.throwUserNotFoundError();
-  }
-
-  private async lookForUser() {
-    this.foundUser = (await User.findByName(this.name))[0];
-  }
-
-  private throwUserNotFoundError() {
-    this.response.status(404).send("User was not found");
-  }
-
-  private async checkCredentials() {
-    if (await this.areCredentialsEqual()) this.sendSuccessResponse();
-    else this.throwCredentialsUnequal();
-  }
-
-  public async areCredentialsEqual(): Promise<boolean> {
-    return await bcrypt.compare(
-      this.password,
-      (this.foundUser as DatabaseUser).password
-    );
-  }
-
-  public sendSuccessResponse() {
-    this.response.status(200).json(this.foundUser);
-  }
-
-  public throwCredentialsUnequal() {
-    this.response.status(403).send("Password is not correct");
-  }
-}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const requestHelper = new RequestHelper(req);
+  class Authorization {
+    private static requestHelper = new RequestHelper(req);
+    private static credentials: Credentials =
+      Authorization.requestHelper.getBody();
 
-  if (requestHelper.isPOST()) {
-    try {
-      const credentials = requestHelper.getBody();
-      const authorization = new Authorization(credentials, res);
-      await mongoose.connect(process.env.MONGODB_HOST as string);
-      await authorization.run();
-    } catch (err) {
-      res.status(500).send("Server could not handle the request");
-    } finally {
-      if (mongoose.connection.readyState === 1) await mongoose.disconnect();
+    public static async run() {
+      await Authorization.checkIfPostMethod();
     }
-  } else res.status(405).send("This endpoint only accepts POST method");
+
+    private static async checkIfPostMethod() {
+      if (Authorization.isPostMethod())
+        await Authorization.connectToDbAndTryToAuthorizate();
+      else Authorization.throwWrongMethodError();
+    }
+
+    private static isPostMethod(): boolean {
+      return Authorization.requestHelper.isPOST();
+    }
+
+    private static throwWrongMethodError() {
+      res.status(405).send("This endpoint only accepts POST method");
+    }
+
+    private static async connectToDbAndTryToAuthorizate() {
+      try {
+        await mongoose.connect(process.env.MONGODB_HOST as string);
+        await Authorization.tryToAuthorizate();
+      } catch (err) {
+        Authorization.throwServerError();
+      } finally {
+        if (mongoose.connection.readyState === 1) await mongoose.disconnect();
+      }
+    }
+
+    private static throwServerError() {
+      res.status(500).send("Server could not handle the request");
+    }
+
+    private static async tryToAuthorizate() {
+      if (Authorization.areCredentialsDefined())
+        await Authorization.verifyAccessToken(
+          Authorization.credentials.accessToken as string
+        );
+      else Authorization.throwCredentialsError();
+    }
+
+    private static areCredentialsDefined(): boolean {
+      const { name, password, accessToken } = Authorization.credentials;
+      console.log(Authorization.credentials);
+
+      return !!name && !!password && !!accessToken;
+    }
+
+    private static throwCredentialsError() {
+      res.status(500).send("Some credential unspecified");
+    }
+
+    private static async verifyAccessToken(accessToken: string) {
+      try {
+        const { _id } = JWT.verifyAccessToken(accessToken);
+        await Authorization.checkIfUserExists(_id);
+      } catch {
+        Authorization.throwAccessTokenError();
+      }
+    }
+
+    private static throwAccessTokenError() {
+      res.status(403).send("Access token expired");
+    }
+
+    private static async checkIfUserExists(userId: string) {
+      const foundUser = await Authorization.lookForUser(userId);
+      if (foundUser) await Authorization.checkCredentials(foundUser.password);
+      else this.throwUserNotFoundError();
+    }
+
+    private static async lookForUser(
+      userId: string
+    ): Promise<DatabaseUser | undefined> {
+      return (await User.findById(userId)) as DatabaseUser | undefined;
+    }
+
+    private static throwUserNotFoundError() {
+      res.status(404).send("User not found");
+    }
+
+    private static async checkCredentials(actualPassword: string) {
+      if (await Authorization.areCredentialsEqual(actualPassword))
+        Authorization.sendSuccessResponse();
+      else Authorization.throwCredentialsUnequalError();
+    }
+
+    private static async areCredentialsEqual(
+      actualPassword: string
+    ): Promise<boolean> {
+      return await bcrypt.compare(
+        Authorization.credentials.password,
+        actualPassword
+      );
+    }
+
+    private static sendSuccessResponse() {
+      res.send("Success");
+    }
+
+    private static throwCredentialsUnequalError() {
+      res.status(401).send("Password is not correct");
+    }
+  }
+
+  await Authorization.run();
 }
--- pages/api/auth/registrate.ts
+++ pages/api/auth/registrate.ts
@@ -5,80 +5,117 @@ import { NextApiRequest, NextApiResponse } from "next";
 import User from "src/models/User";
 import { Credentials } from "src/types/auth";
 import { DatabaseUser } from "src/types/db";
+import JWT from "src/utils/JWT";
 import RequestHelper from "src/utils/RequestHelper";
-
-class Registration {
-  private name: string;
-  private password: string;
-  private response: NextApiResponse;
-  private foundUsers: DatabaseUser[] = [];
-
-  constructor(credentials: Credentials, res: NextApiResponse) {
-    const { name, password } = credentials;
-    this.name = name;
-    this.password = password;
-    this.response = res;
-  }
-
-  public async run() {
-    if (this.areNameAndPasswordDefined()) await this.checkIfUserExists();
-    else this.throwNameOrPasswordUndefinedError();
-  }
-
-  private areNameAndPasswordDefined(): boolean {
-    return !!this.name && !!this.password;
-  }
-
-  private throwNameOrPasswordUndefinedError() {
-    this.response.status(403).send("Either name or password was not provided");
-  }
-
-  private async checkIfUserExists() {
-    await this.lookForUsers();
-    if (this.foundUsers.length > 0) this.throwUserExistsError();
-    else await this.completeRegistration();
-  }
-
-  private async lookForUsers() {
-    this.foundUsers = await User.findByName(this.name);
-  }
-
-  private throwUserExistsError() {
-    this.response.status(409).send("The user already exists");
-  }
-
-  private async completeRegistration() {
-    await this.createUser();
-    this.sendSuccessResponse();
-  }
-
-  private async createUser() {
-    const encryptedPassword = await bcrypt.hash(this.password, 10);
-    const newUser = new User({ name: this.name, password: encryptedPassword });
-    await newUser.save();
-  }
-
-  private sendSuccessResponse() {
-    this.response.status(200).send("Success");
-  }
-}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const requestHelper = new RequestHelper(req);
+  class Registration {
+    private static requestHelper = new RequestHelper(req);
+    private static credentials: Credentials =
+      Registration.requestHelper.getBody();
 
-  if (requestHelper.isPOST()) {
-    try {
-      const credentials = requestHelper.getBody();
-      const registration = new Registration(credentials, res);
-      await mongoose.connect(process.env.MONGODB_HOST as string);
-      await registration.run();
-    } catch (err) {
-      res.status(500).send("Server could not handle the request");
-    } finally {
-      if (mongoose.connection.readyState === 1) await mongoose.disconnect();
+    public static async run() {
+      await Registration.checkIfPostMethod();
     }
-  } else res.status(405).send("This endpoint only accepts POST method");
+
+    private static async checkIfPostMethod() {
+      if (Registration.isPostMethod())
+        await Registration.connectToDbAndTryToRegistrate();
+      else Registration.throwWrongMethodError();
+    }
+
+    private static isPostMethod(): boolean {
+      return Registration.requestHelper.isPOST();
+    }
+
+    private static throwWrongMethodError() {
+      res.status(405).send("This endpoint only accepts POST method");
+    }
+
+    private static async connectToDbAndTryToRegistrate() {
+      try {
+        await mongoose.connect(process.env.MONGODB_HOST as string);
+        await Registration.tryToRegistrate();
+      } catch {
+        Registration.throwServerError();
+      } finally {
+        if (mongoose.connection.readyState === 1) await mongoose.disconnect();
+      }
+    }
+
+    private static async tryToRegistrate() {
+      if (Registration.areCredentialsDefined())
+        await Registration.checkIfUserExists();
+      else Registration.throwCredentialsError();
+    }
+
+    private static throwServerError() {
+      res.status(500).send("Server could not handle the request");
+    }
+
+    private static areCredentialsDefined(): boolean {
+      const { name, password } = Registration.credentials;
+      return !!name && !!password;
+    }
+
+    private static throwCredentialsError() {
+      res.status(500).send("Either name or password was not provided");
+    }
+
+    private static async checkIfUserExists() {
+      const foundUser = await Registration.lookForUsers();
+      if (foundUser) Registration.throwUserExistsError();
+      else await Registration.completeRegistration();
+    }
+
+    private static async lookForUsers(): Promise<DatabaseUser | undefined> {
+      const { name } = Registration.credentials;
+      return (await User.findByName(name))[0] as DatabaseUser | undefined;
+    }
+
+    private static throwUserExistsError() {
+      res.status(409).send("The user already exists");
+    }
+
+    private static async completeRegistration() {
+      const newUser = await Registration.createUser();
+      Registration.sendSuccessResponse(newUser._id as string);
+    }
+
+    private static async createUser(): Promise<DatabaseUser> {
+      const { name, password } = Registration.credentials;
+      const encryptedPassword = await bcrypt.hash(password, 10);
+      const newUser = new User({
+        name,
+        password: encryptedPassword
+      });
+      await newUser.save();
+      return newUser;
+    }
+
+    private static sendSuccessResponse(userId: string) {
+      const { accessToken, refreshToken } = Registration.createTokens(userId);
+      res
+        .status(200)
+        .setHeader(
+          "Set-Cookie",
+          `refreshToken=${refreshToken}; httpOnly; max-age=31556952`
+        )
+        .json({ accessToken });
+    }
+
+    private static createTokens(userId: string): {
+      accessToken: string;
+      refreshToken: string;
+    } {
+      const accessToken = JWT.createAccessToken(userId);
+      const refreshToken = JWT.createRefreshToken(userId);
+      return { accessToken, refreshToken };
+    }
+  }
+
+  await Registration.run();
 }
