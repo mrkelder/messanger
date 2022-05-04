beb19fc7e3b3edc9914db18c9cf954eeda2fa6c7
Vlad A <vageev06@gmail.com>
2022-05-04 14:54:12 +0300

    test: moved tests sing in/up tests to one file since both pages were merged in one

--- __test__/pages/index.test.tsx
+++ __test__/pages/index.test.tsx
@@ -1,6 +1,7 @@
 import { screen, render, fireEvent } from "@testing-library/react";
 import axios from "axios";
-import Regestration from "pages/regestration";
+
+import Home from "pages/index";
 import "@testing-library/jest-dom";
 
 jest.mock("axios");
@@ -10,11 +11,78 @@ const testUser = {
   password: "user-password"
 };
 
+beforeEach(() => {
+  render(<Home />);
+});
+
 describe("Regestration page", () => {
-  beforeEach(() => {
-    render(<Regestration />);
+  test("Should render page with all components", () => {
+    const h1 = screen.getByText(/Authorization/i);
+    const nameInput = screen.getByPlaceholderText(/Name/i);
+    const passwordInput = screen.getByPlaceholderText(/Password/i);
+    const hidePasswordCheckbox = screen.getByRole("checkbox");
+    const submitButton = screen.getByText(/Sign up/i);
+    const linkToRegistration = screen.getByText(/Sign up/i);
+
+    expect(h1).toBeInTheDocument();
+    expect(nameInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(hidePasswordCheckbox).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+    expect(linkToRegistration).toBeInTheDocument();
   });
 
+  test("Should warn that either the name or the password isn't correct", () => {
+    axios.post.mockImplementationOnce(() => Promise.resolve({ status: 403 }));
+
+    const nameInput = screen.getByPlaceholderText(/Name/i);
+    const passwordInput = screen.getByPlaceholderText(/Password/i);
+    const submitButton = screen.getByText(/Sign up/i);
+
+    fireEvent.change(nameInput, { target: { value: testUser.password } });
+    fireEvent.change(passwordInput, { target: { value: testUser.password } });
+    fireEvent.click(submitButton);
+  });
+
+  test("Should reveal password", () => {
+    const passwordInput = screen.getByPlaceholderText(/Password/i);
+    const hidePasswordCheckbox = screen.getByRole("checkbox");
+
+    fireEvent.change(passwordInput, { target: { value: testUser.password } });
+
+    const unrevealedPasswordInput = screen.getByText(
+      testUser.password.replace(/.{1}/, "*")
+    );
+    expect(unrevealedPasswordInput).toBeInTheDocument();
+
+    fireEvent.click(hidePasswordCheckbox);
+
+    const revealedPasswordInput = screen.getByText(testUser.password);
+    expect(revealedPasswordInput).toBeInTheDocument();
+  });
+
+  test("Should warn about the empty name input", () => {
+    const passwordInput = screen.getByPlaceholderText(/Password/i);
+    const submitButton = screen.getByText(/Sign up/i);
+
+    fireEvent.change(passwordInput, { target: { value: testUser.password } });
+    fireEvent.click(submitButton);
+
+    expect(screen.getByText(/Name is empty/i));
+  });
+
+  test("Should warn about the empty password input", () => {
+    const nameInput = screen.getByPlaceholderText(/Name/i);
+    const submitButton = screen.getByText(/Sign up/i);
+
+    fireEvent.change(nameInput, { target: { value: testUser.name } });
+    fireEvent.click(submitButton);
+
+    expect(screen.getByText(/Password is empty/i));
+  });
+});
+
+describe("Regestration state", () => {
   test("Should render page with all components", () => {
     const h1 = screen.getByText(/Registration/i);
     const nameInput = screen.getByPlaceholderText(/Name/i);
