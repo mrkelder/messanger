b899ef5996da6285a4cf76664059e0115242c212
Vlad A <vageev06@gmail.com>
2022-05-06 18:50:27 +0300

    feat: added error snackbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { useState, useCallback } from "react";
 
+import { Close } from "@mui/icons-material";
+import { Alert, IconButton, Snackbar, Stack, Typography } from "@mui/material";
 import axios from "axios";
 import type { NextPage } from "next";
 import Head from "next/head";
@@ -10,12 +12,18 @@ import { Credentials } from "src/types/auth";
 
 const Home: NextPage = () => {
   const [isRegistration, setIsRegistration] = useState(true);
+  const [isAlertOpened, setIsAlertOpened] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("Error message");
   const { Provider: AuthContext } = authContext;
   const title = isRegistration ? "Registration" : "Authorization";
   const buttonText = isRegistration ? "Sign Up" : "Sign In";
   const linkText = isRegistration
     ? "Have the account already?"
     : "Don't have an account yet?";
+
+  const handleAlertClose = () => {
+    setIsAlertOpened(false);
+  };
 
   const registrate = useCallback(async (credentials: Credentials) => {
     const result = await axios.post(
@@ -55,6 +63,29 @@ const Home: NextPage = () => {
           callback={callback}
         />
       </AuthContext>
+
+      <Snackbar
+        open={isAlertOpened}
+        onClose={handleAlertClose}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      >
+        <Alert severity="error" sx={{ width: "100%" }}>
+          <Stack
+            alignItems="center"
+            justifyContent="space-between"
+            direction="row"
+            sx={{ width: "inherit" }}
+          >
+            <Typography>{errorMessage}</Typography>
+            <IconButton
+              sx={{ padding: "0", marginLeft: "3px" }}
+              onClick={handleAlertClose}
+            >
+              <Close />
+            </IconButton>
+          </Stack>
+        </Alert>
+      </Snackbar>
     </>
   );
 };
