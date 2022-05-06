59fd9f20725a840d11cd9c4d03c10b7ef1cbbe02
Vlad A <vageev06@gmail.com>
2022-05-06 12:32:54 +0300

    docs: created a storybook for AuthPageTemplate

---
+++ src/components/AuthPageTemplate/AuthPageTemplate.stories.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { screen, userEvent } from "@storybook/testing-library";
+
+import authContext from "src/contexts/authContext";
+
+import AuthPageTemplate from "./index";
+
+import "styles/globals.css";
+
+const argTypes = {
+  title: {
+    options: ["Registration", "Authorization"],
+    control: { type: "select" }
+  },
+  buttonText: {
+    options: ["Sign Up", "Sign In"],
+    control: { type: "select" }
+  },
+  linkText: {
+    options: ["Have the account already?", "Don't have an account yet?"],
+    control: { type: "select" }
+  }
+};
+
+export default {
+  title: "AuthPageTemplate",
+  component: AuthPageTemplate,
+  argTypes
+} as ComponentMeta<typeof AuthPageTemplate>;
+
+const Template: ComponentStory<typeof AuthPageTemplate> = args => (
+  <authContext.Provider value={{ changePage: () => {} }}>
+    <AuthPageTemplate {...args} />
+  </authContext.Provider>
+);
+
+export const Registration = Template.bind({});
+Registration.args = {
+  title: argTypes.title.options[0],
+  buttonText: argTypes.buttonText.options[0],
+  linkText: argTypes.linkText.options[0]
+};
+
+export const Authorization = Template.bind({});
+Authorization.args = {
+  title: argTypes.title.options[1],
+  buttonText: argTypes.buttonText.options[1],
+  linkText: argTypes.linkText.options[1]
+};
+
+export const InputErrors = Template.bind({});
+
+InputErrors.args = Registration.args;
+
+InputErrors.play = async () => {
+  const nameInput = screen.getByPlaceholderText(/Name/i);
+  const passwordInput = screen.getByPlaceholderText(/Password/i);
+  const submitButton = screen.getByText(/Sign Up/i);
+
+  await userEvent.type(nameInput, "123");
+  await userEvent.type(passwordInput, "12345");
+  await userEvent.click(submitButton);
+  await userEvent.unhover(submitButton);
+};
