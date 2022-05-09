c54ca00105fdc034c6a0ff0e9d7f256b80621316
Vlad A <vageev06@gmail.com>
2022-05-09 11:55:21 +0300

    test: createad registration E2E test

---
+++ cypress/integration/auth/registration.ts
@@ -0,0 +1,30 @@
+describe("Registration", () => {
+  it("Should display input errors", () => {
+    cy.fixture("testUser").then(testUser => {
+      cy.task("db:deleteUser", testUser.name);
+      cy.visit("/");
+      cy.contains("Sign Up").click();
+      cy.contains("Name has to be at least 4 characters long");
+      cy.contains("Password has to be at least 6 characters long");
+    });
+  });
+
+  it("Should successfully sing up", () => {
+    cy.fixture("testUser").then(testUser => {
+      cy.get("input[name=name]").type(testUser.name);
+      cy.get("input[name=password]").type(testUser.password);
+      cy.contains("Sign Up").click();
+      cy.wait(3000);
+    });
+  });
+
+  it("Should throw an error because such user already exists", () => {
+    cy.fixture("testUser").then(testUser => {
+      cy.contains("Sign Up").click();
+      cy.contains("Such user already exists, try another name");
+      cy.task("db:deleteUser", testUser.name);
+    });
+  });
+});
+
+export {};
