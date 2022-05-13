125cdbeddb3533368575cd1bff4fe05b2e655846
Vlad A <vageev06@gmail.com>
2022-05-13 17:40:31 +0300

    test: created test for the main page

---
+++ __test__/pages/m.test.tsx
@@ -0,0 +1,10 @@
+import { screen, render } from "@testing-library/react";
+import M from "pages/m";
+
+describe("Main page", () => {
+  test('Should render header with "main" variant', () => {
+    render(<M />);
+
+    expect(screen.getByText(/Belo Chat/i));
+  });
+});
