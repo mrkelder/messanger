0e735bfdc56d67ba208261015d6133ef4c218fba
Vlad A <vageev06@gmail.com>
2022-05-16 12:01:43 +0300

    test: created tests for useReducer logic with the localStorage

--- __test__/store/userReducer.test.tsx
+++ __test__/store/userReducer.test.tsx
@@ -1,6 +1,9 @@
+import store from "src/store";
+import { USER_DATA_LOCAL_STORAGE_NAME } from "src/store/CONSTANTS";
 import reducer, {
   setAccessToken,
-  setUserData
+  setUserData,
+  initStoreFromLocalStorage
 } from "src/store/reducers/userReducer";
 
 jest.mock("axios");
@@ -9,6 +12,13 @@ const initState = {
   accessToken: "",
   userData: {
     userName: ""
+  }
+};
+
+const localStorageState = {
+  accessToken: "header.payload.secret",
+  userData: {
+    userName: "New User"
   }
 };
 
@@ -32,5 +42,42 @@ describe("User reducer", () => {
       ...initState,
       userData: { userName }
     });
+  });
+});
+
+describe("User reducer with localStorage", () => {
+  test("Should remain the same state because of the invalid user data", () => {
+    localStorage.setItem(
+      USER_DATA_LOCAL_STORAGE_NAME,
+      JSON.stringify({ accessToken: localStorageState.accessToken })
+    );
+
+    store.dispatch(initStoreFromLocalStorage());
+    expect(store.getState().user).toEqual(initState);
+  });
+
+  test("Should remain the same state because of the invalid access token", () => {
+    localStorage.setItem(
+      USER_DATA_LOCAL_STORAGE_NAME,
+      JSON.stringify({ userData: localStorageState.userData })
+    );
+
+    store.dispatch(initStoreFromLocalStorage());
+    expect(store.getState().user).toEqual(initState);
+  });
+
+  test("Should remain the same state because of no data", () => {
+    store.dispatch(initStoreFromLocalStorage());
+    expect(store.getState().user).toEqual(initState);
+  });
+
+  test("Should initiate store from localStorage", () => {
+    localStorage.setItem(
+      USER_DATA_LOCAL_STORAGE_NAME,
+      JSON.stringify(localStorageState)
+    );
+
+    store.dispatch(initStoreFromLocalStorage());
+    expect(store.getState().user).toEqual(localStorageState);
   });
 });
