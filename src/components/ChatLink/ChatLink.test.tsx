801138859f81d5dec3ed60dae30faf04ca1e8338
Vlad A <vageev06@gmail.com>
2022-06-06 10:47:53 +0300

    fix: corrected both ChatLink code and tests

--- src/components/ChatLink/ChatLink.test.tsx
+++ src/components/ChatLink/ChatLink.test.tsx
@@ -1,4 +1,5 @@
 import { screen, render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 import { DELETE_CHAT_EVENT_NAME } from "src/components/CONSTANTS";
 import { Chat } from "src/types/chat";
@@ -28,7 +29,7 @@ const chatWithReadLastMessage: Chat = {
   }
 };
 
-const chatWithUnreadLastMessage: Chat = {
+const chatWith3UnreadMessages: Chat = {
   ...chatWithoutLastMessage,
   lastMessage: {
     _id: "0jb11fbbdlkj2af25d278kf2",
@@ -41,10 +42,18 @@ const chatWithUnreadLastMessage: Chat = {
   countOfUnreadMessages: 3
 };
 
+const chatWith150UnreadMessages: Chat = {
+  ...chatWith3UnreadMessages,
+  countOfUnreadMessages: 150
+};
+
 describe("ChatLink", () => {
   test("Should render a variant with no messages", () => {
     render(<ChatLink chat={chatWithoutLastMessage} userId={user._id} />);
 
+    expect(
+      screen.getByText(peer.name.charAt(0).toUpperCase())
+    ).toBeInTheDocument();
     expect(screen.getByText(peer.name)).toBeInTheDocument();
     expect(screen.queryByText(0)).toBeNull();
   });
@@ -52,39 +61,71 @@ describe("ChatLink", () => {
   test("Should render a variant with the read message", () => {
     render(<ChatLink chat={chatWithReadLastMessage} userId={user._id} />);
 
+    const textRegExp = new RegExp(
+      chatWithReadLastMessage.lastMessage?.text as string,
+      "i"
+    );
+
+    expect(
+      screen.getByText(peer.name.charAt(0).toUpperCase())
+    ).toBeInTheDocument();
     expect(screen.getByText(peer.name)).toBeInTheDocument();
-    expect(screen.getByText(3)).toBeInTheDocument();
+    expect(screen.getByText(textRegExp)).toBeInTheDocument();
+    expect(screen.queryByText(0)).toBeNull();
   });
 
   test("Should render a variant with an unread message", () => {
-    render(<ChatLink chat={chatWithUnreadLastMessage} userId={user._id} />);
+    render(<ChatLink chat={chatWith3UnreadMessages} userId={user._id} />);
+    expect(screen.getByText(3)).toBeInTheDocument();
   });
 
-  test("Should emit a custom event after the component is held for 2 seconds (mobile)", () => {
+  test("Should render a variant with unread 99 messages even thought ther're 150", () => {
+    render(<ChatLink chat={chatWith150UnreadMessages} userId={user._id} />);
+
+    expect(screen.getByText(99)).toBeInTheDocument();
+  });
+
+  // test("Should emit a custom event after the component is held for 1 second (mobile)", async () => {
+  // FIXME: cover touch event emission
+  //
+  //   async function timoutDelay() {
+  //     return new Promise(res => {
+  //       setTimeout(() => {
+  //         res(null);
+  //       }, 1000);
+  //     });
+  //   }
+
+  //   render(<ChatLink chat={chatWithoutLastMessage} userId={user._id} />);
+
+  //   const mockCallback = jest.fn();
+  //   const chatLink = screen.getByRole("button");
+
+  //   addEventListener(DELETE_CHAT_EVENT_NAME, mockCallback);
+
+  //   const touch = new Touch({ clientX: 0, clientY: 0 });
+
+  //   const touchList = new TouchList();
+
+  //   touchList[0] = touch;
+
+  //   fireEvent.touchStart(chatLink, {  });
+
+  //   // await timoutDelay();
+
+  //   expect(mockCallback).toBeCalled();
+  //   removeEventListener(DELETE_CHAT_EVENT_NAME, mockCallback);
+  // });
+
+  test("Should emit a custom event after right click (desktop)", () => {
     render(<ChatLink chat={chatWithoutLastMessage} userId={user._id} />);
 
     const mockCallback = jest.fn();
     const chatName = screen.getByText(peer.name);
 
-    fireEvent.touchStart(chatName);
     addEventListener(DELETE_CHAT_EVENT_NAME, mockCallback);
-
-    setTimeout(() => {
-      removeEventListener(DELETE_CHAT_EVENT_NAME, mockCallback);
-      expect(mockCallback).toBeCalled();
-    }, 2000);
-  });
-
-  test("Should emit a custom event after right click (desktop)", () => {
-    render(<ChatLink chat={chatWithoutLastMessage} />);
-
-    const mockCallback = jest.fn();
-    const chatName = screen.getByText(peer.name);
-
-    addEventListener(DELETE_CHAT_EVENT_NAME, mockCallback);
-    fireEvent.click(chatName);
+    fireEvent.contextMenu(chatName);
     removeEventListener(DELETE_CHAT_EVENT_NAME, mockCallback);
-
-    expect(mockCallback).toBeCalled();
+    ~expect(mockCallback).toBeCalled();
   });
 });
