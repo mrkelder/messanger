f25645aab5562d40e421c79bbd46a400a8f9b1ff
Vlad A <vageev06@gmail.com>
2022-06-06 18:24:10 +0300

    dev: made lastMessage field optional

--- src/types/chat.d.ts
+++ src/types/chat.d.ts
@@ -2,6 +2,6 @@ import { DatabaseMessage, DatabaseChat } from "./db";
 
 export type Chat = Omit<DatabaseChat, "members"> & {
   members: Array<{ _id: string; name: string }>;
-  lastMessage: DatabaseMessage;
+  lastMessage?: DatabaseMessage;
   countOfUnreadMessages: number;
 };
