6b2162aac6ebc3f983a6ca497a67de7543a8c651
Vlad Ageev <vageev06@gmail.com>
2022-08-22 19:31:53 +0300

    test: created tests for GetChatsController and fixed related files accordingly

--- __test__/controllers/auth/AuthControllerTestingUtils.ts
+++ __test__/controllers/auth/AuthControllerTestingUtils.ts
@@ -14,18 +14,25 @@ interface Credentials {
   password: string;
 }
 
+interface GetReq {
+  method: "GET";
+  cookies?: { [key: string]: any };
+}
+
 interface PostReq {
   method: "POST";
   body: { [key: string]: any };
+  cookies?: { [key: string]: any };
 }
 
 interface PutReq {
   method: "PUT";
-  cookies: { [key: string]: any };
+  cookies?: { [key: string]: any };
 }
 
 export class AuthControllerTestingUtils {
   public credentials: Credentials;
+  public getReq: GetReq;
   public postReq: PostReq;
   public putReq: PutReq;
 
@@ -43,6 +50,7 @@ export class AuthControllerTestingUtils {
       password: "some-test-password"
     };
 
+    this.getReq = { method: "GET" };
     this.postReq = { method: "POST", body: this.credentials };
     this.putReq = { method: "PUT", cookies: {} };
   }
--- __test__/controllers/user/GetChatsController.test.ts
+++ __test__/controllers/user/GetChatsController.test.ts
@@ -1,51 +1,135 @@
+import mongoose from "mongoose";
+
+import { GetChatsController } from "src/controllers/user";
+import Chat from "src/models/Chat";
+import User, { UserDocument } from "src/models/User";
+import { DatabaseChat } from "src/types/db";
+import JWT from "src/utils/JWT";
+
+import {
+  AuthControllerTestingUtils,
+  StatusObject
+} from "../auth/AuthControllerTestingUtils";
+
+async function execMongodbOperation<T>(func: () => Promise<T>): Promise<T> {
+  await mongoose.connect(process.env.MONGODB_HOST as string);
+  const result = await func();
+  await mongoose.disconnect();
+  return result;
+}
+
+async function createChat(userId: string) {
+  return execMongodbOperation(async () => {
+    const newChat = new Chat({ members: new mongoose.Types.ObjectId(userId) });
+    await newChat.save();
+  });
+}
+
+async function deleteChat(userId: string) {
+  return execMongodbOperation(async () => {
+    await Chat.deleteOne({
+      members: { $in: [new mongoose.Types.ObjectId(userId)] }
+    });
+  });
+}
+
+async function getChat(userId: string): Promise<DatabaseChat> {
+  return execMongodbOperation<DatabaseChat>(async () => {
+    const chats = await Chat.find({
+      members: {
+        $in: [new mongoose.Types.ObjectId(userId)]
+      }
+    });
+
+    return chats[0];
+  });
+}
+
+async function getUserId(userName: string): Promise<string> {
+  return execMongodbOperation<string>(async () => {
+    const user = (await User.findByName(userName)) as UserDocument;
+    return user.id;
+  });
+}
+
+const testUser = new AuthControllerTestingUtils("get-chats-controller-user");
+
 describe("Chat list", () => {
   beforeAll(async () => {
-    const user = new User(userCredantials);
-    const peer = new User(peerCredantials);
-    const chat = new Chat({ members: [user._id, peer._id] });
-    await user.save();
-    await peer.save();
-    await chat.save();
+    await testUser.createUser();
+    await createChat(await getUserId(testUser.credentials.name));
   });
 
   afterAll(async () => {
-    const user = await User.findByName(userCredantials.name);
-    await Chat.deleteOne({ members: { $in: user?._id } });
-    await User.deleteByName(userCredantials.name);
-    await User.deleteByName(peerCredantials.name);
+    await deleteChat(await getUserId(testUser.credentials.name));
+    await testUser.deleteUser();
   });
 
   test("Should successfully fetch chats", async () => {
-    const { data } = await axios.get(
-      getChatsAPI,
-      returnConf(await getAccessToken())
-    );
+    const userId = await getUserId(testUser.credentials.name);
+    let sO: StatusObject = { status: 200 };
+    const testRes = { ...testUser.res, status: testUser.statusSetter(sO) };
+    const testReq = {
+      ...testUser.getReq,
+      cookies: { accessToken: JWT.createAccessToken(userId) }
+    };
+    const controller = new GetChatsController({
+      req: testReq as any,
+      res: testRes as any
+    });
 
-    expect(data).toBeDefined();
-    expect(data).toHaveLength(1);
+    await controller.run();
+    const chat = await getChat(userId);
+    expect(sO.status).toBe(200);
+    expect(chat).toBeDefined();
   });
 
   test("Should throw an error because accessToken is not passed", async () => {
-    try {
-      await axios.get(getChatsAPI);
-    } catch ({ message }) {
-      expect(message).toMatch("401");
-    }
+    let sO: StatusObject = { status: 200 };
+    const testRes = { ...testUser.res, status: testUser.statusSetter(sO) };
+    const testReq = {
+      ...testUser.getReq,
+      cookies: {}
+    };
+    const controller = new GetChatsController({
+      req: testReq as any,
+      res: testRes as any
+    });
+
+    await controller.run();
+    expect(sO.status).toBe(403);
   });
 
   test("Should throw an error because accessToken is invalid", async () => {
-    try {
-      await axios.get(getChatsAPI, returnBadConf(await getAccessToken()));
-    } catch ({ message }) {
-      expect(message).toMatch("403");
-    }
+    let sO: StatusObject = { status: 200 };
+    const testRes = { ...testUser.res, status: testUser.statusSetter(sO) };
+    const testReq = {
+      ...testUser.getReq,
+      cookies: { accessToken: "xxxxxxx.xxxxxxxxxxx.xxxxxxxxxxxx" }
+    };
+    const controller = new GetChatsController({
+      req: testReq as any,
+      res: testRes as any
+    });
+
+    await controller.run();
+    expect(sO.status).toBe(403);
   });
 
   test("Should throw an error because of invalid http method", async () => {
-    try {
-      await axios.put(getChatsAPI, returnConf(await getAccessToken()));
-    } catch ({ message }) {
-      expect(message).toMatch("405");
-    }
+    const userId = await getUserId(testUser.credentials.name);
+    let sO: StatusObject = { status: 200 };
+    const testRes = { ...testUser.res, status: testUser.statusSetter(sO) };
+    const testReq = {
+      ...testUser.postReq,
+      cookies: { accessToken: JWT.createAccessToken(userId) }
+    };
+    const controller = new GetChatsController({
+      req: testReq as any,
+      res: testRes as any
+    });
+
+    await controller.run();
+    expect(sO.status).toBe(405);
   });
 });
