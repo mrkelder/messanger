3b6fb0fc9e8fdcc46f3301fa13b375cf83cd7ffe
Vlad Ageev <vageev06@gmail.com>
2022-08-24 18:08:06 +0300

    test: refactored GetChatsController tests

--- __test__/controllers/user/GetChatsController.test.ts
+++ __test__/controllers/user/GetChatsController.test.ts
@@ -1,135 +1,84 @@
-import mongoose from "mongoose";
+import { NextApiRequest, NextApiResponse } from "next";
 
 import { GetChatsController } from "src/controllers/user";
-import Chat from "src/models/Chat";
-import User, { UserDocument } from "src/models/User";
-import { DatabaseChat } from "src/types/db";
-import JWT from "src/utils/JWT";
-
 import {
-  AuthControllerTestingUtils,
-  StatusObject
-} from "../auth/AuthControllerTestingUtils";
+  TestCredentialsUtils,
+  TestHttpUtils,
+  TestMongodbUtils
+} from "src/utils/TestUtils";
 
-async function execMongodbOperation<T>(func: () => Promise<T>): Promise<T> {
-  await mongoose.connect(process.env.MONGODB_HOST as string);
-  const result = await func();
-  await mongoose.disconnect();
-  return result;
-}
+const testUser = new TestCredentialsUtils("get-chats-controller-user");
+const resultObject = TestHttpUtils.createReultObject();
 
-async function createChat(userId: string) {
-  return execMongodbOperation(async () => {
-    const newChat = new Chat({ members: new mongoose.Types.ObjectId(userId) });
-    await newChat.save();
-  });
-}
-
-async function deleteChat(userId: string) {
-  return execMongodbOperation(async () => {
-    await Chat.deleteOne({
-      members: { $in: [new mongoose.Types.ObjectId(userId)] }
-    });
-  });
-}
-
-async function getChat(userId: string): Promise<DatabaseChat> {
-  return execMongodbOperation<DatabaseChat>(async () => {
-    const chats = await Chat.find({
-      members: {
-        $in: [new mongoose.Types.ObjectId(userId)]
-      }
-    });
-
-    return chats[0];
-  });
-}
-
-async function getUserId(userName: string): Promise<string> {
-  return execMongodbOperation<string>(async () => {
-    const user = (await User.findByName(userName)) as UserDocument;
-    return user.id;
-  });
-}
-
-const testUser = new AuthControllerTestingUtils("get-chats-controller-user");
+const ids = {
+  userId: ""
+};
 
 describe("Chat list", () => {
   beforeAll(async () => {
-    await testUser.createUser();
-    await createChat(await getUserId(testUser.credentials.name));
+    ids.userId = await TestMongodbUtils.createUser(testUser.getCredentials());
+    await TestMongodbUtils.createChat(ids.userId);
   });
 
   afterAll(async () => {
-    await deleteChat(await getUserId(testUser.credentials.name));
-    await testUser.deleteUser();
+    await TestMongodbUtils.deleteChat(ids.userId);
+    await TestMongodbUtils.deleteUser(testUser.getCredentials().name);
   });
 
   test("Should successfully fetch chats", async () => {
-    const userId = await getUserId(testUser.credentials.name);
-    let sO: StatusObject = { status: 200 };
-    const testRes = { ...testUser.res, status: testUser.statusSetter(sO) };
-    const testReq = {
-      ...testUser.getReq,
-      cookies: { accessToken: JWT.createAccessToken(userId) }
-    };
+    const { userId } = ids;
+    const testReq = TestHttpUtils.createRequest("GET", userId);
+    const testRes = TestHttpUtils.createResponse(resultObject);
+
     const controller = new GetChatsController({
-      req: testReq as any,
-      res: testRes as any
+      req: testReq as NextApiRequest,
+      res: testRes as unknown as NextApiResponse
     });
 
     await controller.run();
-    const chat = await getChat(userId);
-    expect(sO.status).toBe(200);
+    const chat = await TestMongodbUtils.getChat(userId);
+    expect(resultObject.status).toBe(200);
     expect(chat).toBeDefined();
   });
 
   test("Should throw an error because accessToken is not passed", async () => {
-    let sO: StatusObject = { status: 200 };
-    const testRes = { ...testUser.res, status: testUser.statusSetter(sO) };
-    const testReq = {
-      ...testUser.getReq,
-      cookies: {}
-    };
+    const testReq = TestHttpUtils.createRequest("GET");
+    const testRes = TestHttpUtils.createResponse(resultObject);
+
     const controller = new GetChatsController({
-      req: testReq as any,
-      res: testRes as any
+      req: testReq as NextApiRequest,
+      res: testRes as unknown as NextApiResponse
     });
 
     await controller.run();
-    expect(sO.status).toBe(403);
+    expect(resultObject.status).toBe(403);
   });
 
   test("Should throw an error because accessToken is invalid", async () => {
-    let sO: StatusObject = { status: 200 };
-    const testRes = { ...testUser.res, status: testUser.statusSetter(sO) };
-    const testReq = {
-      ...testUser.getReq,
-      cookies: { accessToken: "xxxxxxx.xxxxxxxxxxx.xxxxxxxxxxxx" }
-    };
+    const testReq = TestHttpUtils.createRequest("GET");
+    const testRes = TestHttpUtils.createResponse(resultObject);
+    testReq.cookies.accessToken = "xxxxxxx.xxxxxxxxxxx.xxxxxxxxxxxx";
+
     const controller = new GetChatsController({
-      req: testReq as any,
-      res: testRes as any
+      req: testReq as NextApiRequest,
+      res: testRes as unknown as NextApiResponse
     });
 
     await controller.run();
-    expect(sO.status).toBe(403);
+    expect(resultObject.status).toBe(403);
   });
 
   test("Should throw an error because of invalid http method", async () => {
-    const userId = await getUserId(testUser.credentials.name);
-    let sO: StatusObject = { status: 200 };
-    const testRes = { ...testUser.res, status: testUser.statusSetter(sO) };
-    const testReq = {
-      ...testUser.postReq,
-      cookies: { accessToken: JWT.createAccessToken(userId) }
-    };
+    const { userId } = ids;
+    const testReq = TestHttpUtils.createRequest("POST", userId);
+    const testRes = TestHttpUtils.createResponse(resultObject);
+
     const controller = new GetChatsController({
-      req: testReq as any,
-      res: testRes as any
+      req: testReq as NextApiRequest,
+      res: testRes as unknown as NextApiResponse
     });
 
     await controller.run();
-    expect(sO.status).toBe(405);
+    expect(resultObject.status).toBe(405);
   });
 });
