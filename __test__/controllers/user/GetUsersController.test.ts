9f0f7b46f0e854dbbaf3737b3fce342cad03b0dc
Vlad Ageev <vageev06@gmail.com>
2022-08-25 14:20:07 +0300

    test: refactored GetUsersController tests

--- __test__/controllers/user/GetUsersController.test.ts
+++ __test__/controllers/user/GetUsersController.test.ts
@@ -1,182 +1,134 @@
-import mongoose from "mongoose";
+import { NextApiRequest, NextApiResponse } from "next";
 
 import { GetUsersController } from "src/controllers/user";
-import User, { UserDocument } from "src/models/User";
-import JWT from "src/utils/JWT";
-
 import {
-  AuthControllerTestingUtils,
-  StatusObject
-} from "../auth/AuthControllerTestingUtils";
-
-async function execMongodbOperation<T>(func: () => Promise<T>): Promise<T> {
-  await mongoose.connect(process.env.MONGODB_HOST as string);
-  const result = await func();
-  await mongoose.disconnect();
-  return result;
-}
-
-async function getUserId(userName: string): Promise<string> {
-  return execMongodbOperation<string>(async () => {
-    const user = (await User.findByName(userName)) as UserDocument;
-    return user.id;
-  });
-}
+  TestCredentialsUtils,
+  TestMongodbUtils,
+  TestHttpUtils
+} from "src/utils/TestUtils";
 
 describe("Get users successful responses", () => {
-  const testUser = new AuthControllerTestingUtils("get-users-controller-user");
-  const testPeer = new AuthControllerTestingUtils("get-users-controller-peer");
+  const testUser = new TestCredentialsUtils("get-users-controller-user");
+  const testPeer = new TestCredentialsUtils("get-users-controller-peer");
+  const resultObject = TestHttpUtils.createReultObject();
+
+  const ids = {
+    userId: "",
+    peerId: ""
+  };
 
   beforeEach(async () => {
-    await testUser.createUser();
-    await testPeer.createUser();
+    ids.userId = await TestMongodbUtils.createUser(testUser.getCredentials());
+    ids.peerId = await TestMongodbUtils.createUser(testPeer.getCredentials());
   });
 
   afterEach(async () => {
-    await testUser.deleteUser();
-    await testPeer.deleteUser();
+    await TestMongodbUtils.deleteUser(testUser.getCredentials().name);
+    await TestMongodbUtils.deleteUser(testPeer.getCredentials().name);
   });
 
   test("Should return the user", async () => {
-    let sO: StatusObject = { status: 200, data: [] };
-    const userId = await getUserId(testUser.credentials.name);
-    const testRes = {
-      ...testUser.res,
-      status: testUser.statusSetter(sO),
-      json: testUser.resultDataSetter(sO)
-    };
-    const testReq = {
-      ...testPeer.getReq,
-      query: { userName: testPeer.credentials.name },
-      cookies: { accessToken: JWT.createAccessToken(userId) }
-    };
+    const { userId } = ids;
+    const testReq = TestHttpUtils.createRequest("GET", userId);
+    const testRes = TestHttpUtils.createResponse(resultObject);
+    testReq.query.userName = testPeer.getCredentials().name;
+
     const controller = new GetUsersController({
-      req: testReq as any,
-      res: testRes as any
+      req: testReq as NextApiRequest,
+      res: testRes as unknown as NextApiResponse
     });
 
     await controller.run();
-    expect(sO.data).toHaveLength(1);
+    expect(resultObject.data).toHaveLength(1);
   });
 
   test("Should return the user despite the incompleteness of the name", async () => {
-    let sO: StatusObject = { status: 200, data: [] };
-    const userId = await getUserId(testUser.credentials.name);
-    const testRes = {
-      ...testUser.res,
-      status: testUser.statusSetter(sO),
-      json: testUser.resultDataSetter(sO)
-    };
-    const testReq = {
-      ...testPeer.getReq,
-      query: { userName: testPeer.credentials.name.substring(5) },
-      cookies: { accessToken: JWT.createAccessToken(userId) }
-    };
+    const { userId } = ids;
+    const testReq = TestHttpUtils.createRequest("GET", userId);
+    const testRes = TestHttpUtils.createResponse(resultObject);
+    testReq.query.userName = testPeer.getCredentials().name.substring(5);
+
     const controller = new GetUsersController({
-      req: testReq as any,
-      res: testRes as any
+      req: testReq as NextApiRequest,
+      res: testRes as unknown as NextApiResponse
     });
 
     await controller.run();
-    expect(sO.data).toHaveLength(1);
+    expect(resultObject.data).toHaveLength(1);
   });
 
   test("Should return the user despite the casing", async () => {
-    let sO: StatusObject = { status: 200, data: [] };
-    const userId = await getUserId(testUser.credentials.name);
-    const testRes = {
-      ...testUser.res,
-      status: testUser.statusSetter(sO),
-      json: testUser.resultDataSetter(sO)
-    };
-    const testReq = {
-      ...testPeer.getReq,
-      query: { userName: testPeer.credentials.name.toUpperCase() },
-      cookies: { accessToken: JWT.createAccessToken(userId) }
-    };
+    const { userId } = ids;
+    const testReq = TestHttpUtils.createRequest("GET", userId);
+    const testRes = TestHttpUtils.createResponse(resultObject);
+    testReq.query.userName = testPeer.getCredentials().name.toUpperCase();
+
     const controller = new GetUsersController({
-      req: testReq as any,
-      res: testRes as any
+      req: testReq as NextApiRequest,
+      res: testRes as unknown as NextApiResponse
     });
 
     await controller.run();
-    expect(sO.data).toHaveLength(1);
+    expect(resultObject.data).toHaveLength(1);
   });
 });
 
 describe("Get users failure responses", () => {
-  const testUser = new AuthControllerTestingUtils(
-    "create-chat-controller-user-2"
+  const testUser = new TestCredentialsUtils("get-users-controller-user-2");
+  const testDeletedUser = new TestCredentialsUtils(
+    "get-users-controller-deleted-user-2"
   );
-  const testDeletedUser = new AuthControllerTestingUtils("some-unreal-name");
+  const resultObject = TestHttpUtils.createReultObject();
+
+  const ids = {
+    userId: ""
+  };
 
   beforeAll(async () => {
-    await testUser.createUser();
-    await testDeletedUser.deleteUser();
+    ids.userId = await TestMongodbUtils.createUser(testUser.getCredentials());
+    await TestMongodbUtils.deleteUser(testDeletedUser.getCredentials().name);
   });
 
   afterAll(async () => {
-    await testUser.deleteUser();
+    await TestMongodbUtils.deleteUser(testUser.getCredentials().name);
   });
 
   test("Should throw no results error", async () => {
-    let sO: StatusObject = { status: 200 };
-    const userId = await getUserId(testUser.credentials.name);
-    const testRes = {
-      ...testUser.res,
-      status: testUser.statusSetter(sO),
-      json: testUser.resultDataSetter(sO)
-    };
-    const testReq = {
-      ...testUser.getReq,
-      query: { userName: testDeletedUser.credentials.name },
-      cookies: { accessToken: JWT.createAccessToken(userId) }
-    };
+    const { userId } = ids;
+    const testReq = TestHttpUtils.createRequest("GET", userId);
+    const testRes = TestHttpUtils.createResponse(resultObject);
+    testReq.query.userName = testDeletedUser.getCredentials().name;
+
     const controller = new GetUsersController({
-      req: testReq as any,
-      res: testRes as any
+      req: testReq as NextApiRequest,
+      res: testRes as unknown as NextApiResponse
     });
 
     await controller.run();
-    expect(sO.status).toBe(404);
+    expect(resultObject.status).toBe(404);
   });
 
   test("Should throw no results error because the user and the search names match", async () => {
-    let sO: StatusObject = { status: 200 };
-    const userId = await getUserId(testUser.credentials.name);
-    const testRes = {
-      ...testUser.res,
-      status: testUser.statusSetter(sO),
-      json: testUser.resultDataSetter(sO)
-    };
-    const testReq = {
-      ...testUser.getReq,
-      query: { userName: testUser.credentials.name },
-      cookies: { accessToken: JWT.createAccessToken(userId) }
-    };
+    const { userId } = ids;
+    const testReq = TestHttpUtils.createRequest("GET", userId);
+    const testRes = TestHttpUtils.createResponse(resultObject);
+    testReq.query.userName = testUser.getCredentials().name;
+
     const controller = new GetUsersController({
-      req: testReq as any,
-      res: testRes as any
+      req: testReq as NextApiRequest,
+      res: testRes as unknown as NextApiResponse
     });
 
     await controller.run();
-    expect(sO.status).toBe(404);
+    expect(resultObject.status).toBe(404);
   });
 
   test("Should throw an error because of an unspecified search name", async () => {
-    let sO: StatusObject = { status: 200 };
     try {
-      const userId = await getUserId(testUser.credentials.name);
-      const testRes = {
-        ...testUser.res,
-        status: testUser.statusSetter(sO),
-        json: testUser.resultDataSetter(sO)
-      };
-      const testReq = {
-        ...testUser.getReq,
-        query: {},
-        cookies: { accessToken: JWT.createAccessToken(userId) }
-      };
+      const { userId } = ids;
+      const testReq = TestHttpUtils.createRequest("GET", userId);
+      const testRes = TestHttpUtils.createResponse(resultObject);
+
       const controller = new GetUsersController({
         req: testReq as any,
         res: testRes as any
@@ -184,26 +136,19 @@ describe("Get users failure responses", () => {
 
       await controller.run();
     } catch {
-      expect(sO.status).toBe(500);
+      expect(resultObject.status).toBe(500);
     } finally {
-      expect(sO.status).toBe(500);
+      expect(resultObject.status).toBe(500);
     }
   });
 
   test("Should throw an error because of the multiple search name queries", async () => {
-    let sO: StatusObject = { status: 200 };
     try {
-      const userId = await getUserId(testUser.credentials.name);
-      const testRes = {
-        ...testUser.res,
-        status: testUser.statusSetter(sO),
-        json: testUser.resultDataSetter(sO)
-      };
-      const testReq = {
-        ...testUser.getReq,
-        query: { userName: ["name", "name"] },
-        cookies: { accessToken: JWT.createAccessToken(userId) }
-      };
+      const { userId } = ids;
+      const testReq = TestHttpUtils.createRequest("GET", userId);
+      const testRes = TestHttpUtils.createResponse(resultObject);
+      testReq.query.userName = ["name", "name"] as any;
+
       const controller = new GetUsersController({
         req: testReq as any,
         res: testRes as any
@@ -211,73 +156,52 @@ describe("Get users failure responses", () => {
 
       await controller.run();
     } catch {
-      expect(sO.status).toBe(500);
+      expect(resultObject.status).toBe(500);
     } finally {
-      expect(sO.status).toBe(500);
+      expect(resultObject.status).toBe(500);
     }
   });
 
   test("Should throw an error because accessToken is not passed", async () => {
-    let sO: StatusObject = { status: 200 };
-    const testRes = {
-      ...testUser.res,
-      status: testUser.statusSetter(sO),
-      json: testUser.resultDataSetter(sO)
-    };
-    const testReq = {
-      ...testUser.getReq,
-      query: { userName: testDeletedUser.credentials.name },
-      cookies: {}
-    };
+    const testReq = TestHttpUtils.createRequest("GET");
+    const testRes = TestHttpUtils.createResponse(resultObject);
+
     const controller = new GetUsersController({
-      req: testReq as any,
-      res: testRes as any
+      req: testReq as NextApiRequest,
+      res: testRes as unknown as NextApiResponse
     });
 
     await controller.run();
-    expect(sO.status).toBe(403);
+    expect(resultObject.status).toBe(403);
   });
 
   test("Should throw an error because accessToken is invalid", async () => {
-    let sO: StatusObject = { status: 200 };
-    const testRes = {
-      ...testUser.res,
-      status: testUser.statusSetter(sO),
-      json: testUser.resultDataSetter(sO)
-    };
-    const testReq = {
-      ...testUser.getReq,
-      query: { userName: testDeletedUser.credentials.name },
-      cookies: { accessToken: "xxxxxxxxxxxx.xxxxxx" }
-    };
+    const testReq = TestHttpUtils.createRequest("GET");
+    const testRes = TestHttpUtils.createResponse(resultObject);
+    testReq.cookies.accessToken = "xxxxxxxxxxxxxx.xxxxxxxxxx";
+    testReq.query.userName = testDeletedUser.getCredentials().name;
+
     const controller = new GetUsersController({
-      req: testReq as any,
-      res: testRes as any
+      req: testReq as NextApiRequest,
+      res: testRes as unknown as NextApiResponse
     });
 
     await controller.run();
-    expect(sO.status).toBe(403);
+    expect(resultObject.status).toBe(403);
   });
 
   test("Should throw an error because of invalid http method", async () => {
-    let sO: StatusObject = { status: 200 };
-    const userId = await getUserId(testUser.credentials.name);
-    const testRes = {
-      ...testUser.res,
-      status: testUser.statusSetter(sO),
-      json: testUser.resultDataSetter(sO)
-    };
-    const testReq = {
-      ...testUser.postReq,
-      query: { userName: testDeletedUser.credentials.name },
-      cookies: { accessToken: JWT.createAccessToken(userId) }
-    };
+    const { userId } = ids;
+    const testReq = TestHttpUtils.createRequest("POST", userId);
+    const testRes = TestHttpUtils.createResponse(resultObject);
+    testReq.query.userName = testDeletedUser.getCredentials().name;
+
     const controller = new GetUsersController({
-      req: testReq as any,
-      res: testRes as any
+      req: testReq as NextApiRequest,
+      res: testRes as unknown as NextApiResponse
     });
 
     await controller.run();
-    expect(sO.status).toBe(405);
+    expect(resultObject.status).toBe(405);
   });
 });
