d2ce882dff1a974ea4310c84878cb38340db06c2
Vlad Ageev <vageev06@gmail.com>
2022-08-30 21:14:08 +0300

    feat: added a middleware to take care of authentication

--- src/controllers/socket/SocketServer.ts
+++ src/controllers/socket/SocketServer.ts
@@ -1,8 +1,21 @@
 import { Server } from "socket.io";
 
+import JWT from "src/utils/JWT";
+
 export class SocketServer {
+  static createMiddlewares(io: Server) {
+    io.use((socket, next) => {
+      try {
+        JWT.verifyAccessToken(socket.handshake.auth.token);
+        next();
+      } catch (err) {
+        next(new Error((err as unknown as Error).message));
+      }
+    });
+  }
+
   static createConnection(io: Server) {
-    io.on("connection", async () => {});
+    io.on("connection", async socket => {});
   }
 
   static removeListeners(io: Server) {
