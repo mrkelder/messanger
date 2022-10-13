1790a7bd4598ff19c0257b3f1a68fd8cdfaf22de
Vlad Ageev <vageev06@gmail.com>
2022-10-13 11:45:37 +0300

    feat: created an empty GetMessagesController and the empty tests for it

---
+++ __test__/controllers/chat/GetMessagesController.test.ts
@@ -0,0 +1,8 @@
+import { GetMessagesController } from "src/controllers/chat";
+
+describe("Get chat messages controller", () => {
+  test("Should return last 10 messages", () => {});
+  test("Should return the next 10 messages", () => {});
+  test("Should return last 10 messages inspite of a negative messages offset", () => {});
+  test("Should return an empty array due to requesting the messages that don't exist", () => {});
+});
